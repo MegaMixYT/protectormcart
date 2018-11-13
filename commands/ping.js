73a486f3373f2661909f80c94fe671c1bb3f75fb
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-13 18:51:17 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   const m = await message.channel.send("Измерение Ждите :satellite_orbital: ");
-    m.edit(`${message.author}, ваш пинг** ${m.createdTimestamp - message.createdTimestamp}** мс   Пинг**${Math.round(client.ping)}**мс`);
+    m.edit(`${message.author}, ваш пинг** ${m.createdTimestamp - message.createdTimestamp}** мс`);
 }
 module.exports.help = {
     name: "ping"

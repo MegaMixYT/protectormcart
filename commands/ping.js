152f41fea2d02dc580be2e6d3afc9635f471ac9c
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-13 18:42:00 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const gg = await message.channel.send('Измерение пинга ⏳');
     setTimeout(() => {
-        gg.edit(`${message.member}, ваш пинг! ${bot.ping}мс`)
+        gg.edit(`${message.member}, ваш пинг! ${m.createdTimestamp - message.createdTimestamp}мс`)
       }, 4000);
 }
 module.exports.help = {

1a82585c54749e9b62850b702c210130d4a44406
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-13 19:04:43 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`${message.author}, ваш пинг** ${message.author.ping** мс`);
+    message.channel.send(`${message.author}, ваш пинг** ${message.author.ping}** мс`);
 }
 module.exports.help = {
     name: "ping"

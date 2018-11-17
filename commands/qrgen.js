becac673beeb10982ed9d067945a6fe864119711
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-17 12:37:27 +0000

    Update qrgen.js

--- commands/qrgen.js
+++ commands/qrgen.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 var text = args.join(" ");
-var uri = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
+var uri = `https://api.qrserver.com/v1/create-qr-code/?size=900x900&data=${text}`.replace(/ /g,"%20");
 var imgg = encodeURIComponent(uri);
 let emb = new Discord.RichEmbed()
 .setColor('00ff54')

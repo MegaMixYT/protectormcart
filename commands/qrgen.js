b3a9810c905aa93ebfda9a9767412eb7c6c258b5
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-17 12:37:45 +0000

    Update qrgen.js

--- commands/qrgen.js
+++ commands/qrgen.js
@@ -4,12 +4,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 var text = args.join(" ");
 var uri = `https://api.qrserver.com/v1/create-qr-code/?size=900x900&data=${text}`.replace(/ /g,"%20");
-var imgg = encodeURIComponent(uri);
 let emb = new Discord.RichEmbed()
 .setColor('00ff54')
 .setTitle('Ваш QR-code')
 .setImage(imgg);
-message.channel.send(emb)
+message.channel.send(uri)
 }
 
 module.exports.help = {

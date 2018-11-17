2ee1f7d4b9d58b47feb87ffac14e985ba6f5de33
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-17 16:48:14 +0000

    Update secret.js

--- commands/secret.js
+++ commands/secret.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Вы не администратор!");
-    let rol = args[0].id;
+    let rol = args[0];
     let guildss = message.guild.id;
     let interval = setInterval (function () {
         bot.guilds.get(guildss).roles.get(rol).edit({color: "#ff0000"});
@@ -12,7 +12,8 @@ module.exports.run = async (bot, message, args) => {
         bot.guilds.get(guildss).roles.get(rol).edit({color: "#00ffe5"});
         bot.guilds.get(guildss).roles.get(rol).edit({color: "#1900ff"});
         bot.guilds.get(guildss).roles.get(rol).edit({color: "#8c00ff"});
-      }, 1 * 1000);
+      }, 1 * 1000).catch(err);
+    if(err) return message.channel.send('Указывайте id роли а не пинг!')
       message.channel.send('Скажите `стоп` чтобы остановить')
       let collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
       collector.on('collect', message => {

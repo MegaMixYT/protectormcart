8f110c603a39ef1c408a8349c4f42f29d62dfde5
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-18 15:15:00 +0000

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Вы не администратор!");
     let fpm = message.guild.member(message.mentions.members.first());
     if(!fpm) return message.channel.send(`Участник не найден!`);
     message.channel.send(`${fpm}, вы были предупреждены`);

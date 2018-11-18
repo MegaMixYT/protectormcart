10cd262228fd297510987423758669143151460a
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-18 14:50:45 +0000

    Update developer.js

--- commands/developer.js
+++ commands/developer.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.reply("Привет я бот Protector, меня сделал MCArTyR!");
+    message.reply("Привет я бот Protector, меня сделал MCArTyR специально для сервера World Of Music!");
 }
 
 module.exports.help = {

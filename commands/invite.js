bde45ef1b46a453c220a75cd14bdbda53e4e48c3
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-19 17:43:45 +0300

    Add files via upload

---
+++ commands/invite.js
@@ -0,0 +1,28 @@
+﻿const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    var answers = [
+        "#ff0000",
+        "#ff7700",
+        "#eeff00",
+        "#00ff08",
+        "#00ffe5",
+        "#1900ff",
+        "#8c00ff"
+      ] 
+      var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+      let hEmbed = new Discord.RichEmbed()
+      .setThumbnail(message.author.displayAvatarURL)
+      .setColor(randomAnswer)
+       .setTitle('Список команд в чат:')
+       .addField('Пригласить бота', 'https://bit.ly/protectori', false)
+       .addField('Оффициальный сервер', 'https://discord.gg/bk3stFj', false)
+       .setFooter(`Protector by MCArTyR`, message.author.displayAvatarURL);
+      message.channel.send(hEmbed);
+}
+
+
+
+module.exports.help ={
+    name: "invite"
+}

beba0b582e8d94a4e6cba82b02a1e3d0edd9befc
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-21 16:36:49 +0000

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 let prefixs = require('../prefix.json');
+let fs = require('fs');
 
 module.exports.run = async (bot, message, args) => {
 let prefixss = args[0];

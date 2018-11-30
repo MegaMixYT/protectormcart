633526bc74c8ea8fd3377646c257850312f8c339
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-11-30 21:56:48 +0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,6 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     if(message.author.id !== "355384371362136075") return;
+    function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
     try {
       const code = args.join(" ");
       let evaled = eval(code);
@@ -11,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
  
       message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
 }
 module.exports.help = {

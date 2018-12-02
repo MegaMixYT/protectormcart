ac4b220131628d70f2004efe3b1cd28f876bda76
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-02 16:32:26 +0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     if(message.author.id !== "355384371362136075") return;
     function clean(text) {
   if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)).replace("Promise { <pending> }", / /);
   else
       return text;
 }
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
       if (typeof evaled !== "string")
         evaled = require("util").inspect(evaled);
         
-      message.channel.send(code, {code:"xl"});
+      message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }

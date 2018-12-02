ed0dfc1a8ed032bdb3e5975cb2da67db44927d99
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-02 15:35:49 +0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -12,7 +12,8 @@ module.exports.run = async (bot, message, args) => {
       const code = args.join(" ");
       let evaled = eval(code);
  
-
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
  
       message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {

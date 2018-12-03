1d03f9230926383fbe3711902fb6d37e9f0f651b
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-03 22:33:01 +0300

    Update developer.js

--- commands/developer.js
+++ commands/developer.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.reply("Привет я бот DydleBoat, меня сделал MCArTyR (Идея от _vaenyX_#7280)!");
+    message.reply(`Привет я бот ${bot.user.username}, меня сделал MCArTyR (Идея от _vaenyX_#7280)!`);
 }
 
 module.exports.help = {

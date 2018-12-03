5c730ec6b62be9b42700c2f1ac37bb61be7c9eb0
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-03 22:21:00 +0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,38 +1,25 @@
 const Discord = require("discord.js");
-const inspect = require("inspect");
 
 module.exports.run = async (bot, message, args) => {
     if(message.author.id !== "355384371362136075") return;
-const code = args.join(" ").replace(process.env.TOKEN, 'huy tebe');
-        const token = bot.token.split("").join("[^]{0,2}");
-        const rev = bot.token.split("").reverse().join("[^]{0,2}");
-        const filter = new RegExp(`${token}|${rev}`, "g");
-        try {
-            let output = eval(code);
-            if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
-            output = inspect(output, { depth: 0, maxArrayLength: null });
-            output = output.replace(filter, "[TOKEN]");
-            output = clean(output);
-            if (output.length < 1950) {
-                //Отправляет пользователю данные эмуляции.
-                message.author.send(`\`\`\`js\n${output}\n\`\`\``);
-                //Ставит реакцию (выполнено).
-                message.react("✅")
-            } else {
-                message.author.send(`${output}`, {split:"\n", code:"js"});
-            }
-        } catch (error) {
-            //Захватывает ошибку и говорит об этом.
-            message.channel.send(`Error \`\`\`js\n${error}\`\`\``);
-            //Ставит реакцию (Ошибка).
-            message.react("❎")
-        }
-
-        function clean(text)  {
-            return text
-                .replace(/`/g, "`" + String.fromCharCode(8203))
-                .replace(/@/g, "@" + String.fromCharCode(8203));
-        }
+    function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+        if(evaled === "Promise { <pending> }") return;
+        
+     const gg = await message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
 }
 module.exports.help = {
     name: "eval"

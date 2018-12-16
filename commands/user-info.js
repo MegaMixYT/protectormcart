7d9c48e91e064181e443bb8d1e342bfc6d208785
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-16 19:49:43 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -40,12 +40,13 @@ let r = new Discord.RichEmbed()
 .setThumbnail(mem.displayAvatarURL)
 .setFooter(`ID: ${mem.id}`);
  let gamet
-if (mem.presence.game.type == 0) gamet = `Играет в`
-else if (mem.presence.game.type == 1) gamet = `Стримит`
-else if (mem.presence.game.type == 2) gamet = `Слушает`
-else if (mem.presence.game.type == 3) gamet = `Смотрит`;
+if (mem.presence.game.type == 0) gamet = `Играет в ${}`
+else if (mem.presence.game.type == 1) gamet = `Стримит **${mem.presence.game.name}**`
+else if (mem.presence.game.type == 2) gamet = `Слушает **${mem.presence.game.name}**`
+else if (mem.presence.game.type == 3) gamet = `Смотрит **${mem.presence.game.name}**`
+else if (!mem.presence.game.type) gamet = `Ни во-что не играет`;
 if(mem.presence.status == 'online')
-{ let sts = 'В сети';r.setTitle(`${mem.tag} | ${mem.gamet} ${sts}`);}else if(mem.presence.status == 'idle')
+{ let sts = 'В сети';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(mem.presence.status == 'idle')
 { let sts = 'Не активен';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(mem.presence.status == 'dnd')
 { let sts = 'Не беспокоить';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(mem.presence.status == 'offline')
 { let sts = 'Не в сети';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(!mem.presence.game) {;r.setTitle(`${mem.tag} | Не во что не играет`);}else{};

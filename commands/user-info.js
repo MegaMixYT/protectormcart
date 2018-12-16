6b2ee2adc54cea57af02b33c630a529f25ba125c
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-16 20:10:20 +0300

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -46,7 +46,7 @@ else if (mem.presence.game.type == 1) gamet = `Стримит **${mem.presence.g
 else if (mem.presence.game.type == 2) gamet = `Слушает **${mem.presence.game.name}**`
 else if (mem.presence.game.type == 3) gamet = `Смотрит **${mem.presence.game.name}**`;
 if(mem.presence.status == 'online')
-{ let sts = 'В сети';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(mem.presence.status == 'idle')
+{ let sts = 'В сети';r.setTitle(`${mem.tag} | ${sts} | ${gamet}`);}else if(mem.presence.status == 'idle')
 { let sts = 'Не активен';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(mem.presence.status == 'dnd')
 { let sts = 'Не беспокоить';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(mem.presence.status == 'offline')
 { let sts = 'Не в сети';r.setTitle(`${mem.tag} | ${sts} ${gamet}`);}else if(!mem.presence.game) {;r.setTitle(`${mem.tag} | Не во что не играет`);}else{};

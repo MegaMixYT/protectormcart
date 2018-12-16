6be517fa1ed2d171804e4998c4ff3a604ea84ec1
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-16 23:35:19 +0300

    Update prunememb.js

--- commands/prunememb.js
+++ commands/prunememb.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot,message,args) => {
 let g = args[0];
 if(!g) return message.channel.send('Укажите кол-во дней!')
-message.guild.pruneMembers(args[0], true, `For ${g}days of inactivity`);
+message.guild.pruneMembers('${g}', true, `For ${g}days of inactivity`);
 }
 
 module.exports.help = {

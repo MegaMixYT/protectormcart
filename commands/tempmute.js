60edd70406921743a1662f6f1e341b28ed4c32ed
HickleyDonate <artyr20072014@gmail.com>
2018-12-24 20:42:23 +0300

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
 let tomute = message.mentions.members.first() || message.guild.members.get(args[0]);
 if(!tomute) return message.reply("Пользователь не найден!");
 if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Я не могу замутить этого пользователя!");
-if(!message.author.hasPermission("MANAGE_MESSAGES")) return message.reply("У вас нет права `MANAGE_MESSAGES`!");
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("У вас нет права `MANAGE_MESSAGES`!");
 let muterole = message.guild.roles.find(`name`, "muted");
 //start of create role
 if(!muterole){

86dcb19e8dc40aaf83aad3440aa61aafdb513985
hickleydonate <42618704+hickleydonate@users.noreply.github.com>
2018-12-26 20:51:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -86,12 +86,18 @@ bot.on('message', async message => {
 if(message.author.bot) return;
 if(message.guild.id === '522465512232321044'){
 if(message.channel.id === '527526287565258753'){
-vswk.edit(message.author.tag, message.author.displayAvatarURL).then(vswk.send(message.content));
+vswk.edit(message.author.tag, message.author.displayAvatarURL);
+setTimeout(() => {
+vswk.send(message.content);
+}, 1000);
 }
 }
 if(message.guild.id === '513729490518671360'){
 if(message.channel.id === '527522065763532835'){
-mbwk.edit(message.author.tag, message.author.displayAvatarURL).then(mbwk.send(message.content));
+mbwk.edit(message.author.tag, message.author.displayAvatarURL);
+setTimeout(() => {
+mbwk.send(message.content);
+}, 1000);
 }
 }
 });
